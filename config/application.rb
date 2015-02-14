eaca2856d03c78925dea4ce83741230de8bf360f
rnjai <rnjailamba12@gmail.com>
2015-02-14 16:13:34 +0530

    Added the competitions scaffold

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module Protone1
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -23,5 +24,6 @@ module Protone1
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.initialize_on_precompile = false
+    config.generators.assets = false
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
   
+  resources :competitions
+
+  devise_for :users
   root 'pages#home'
 
 
