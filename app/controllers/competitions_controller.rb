043fb0be64aa13f09ec6b9d2a88cda1408415a38
rnjai <rnjailamba12@gmail.com>
2015-02-15 08:34:44 +0530

    All controllers done - nothing broken

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -1,76 +1,63 @@
 class CompetitionsController < ApplicationController
+  after_filter :store_location
   before_action :set_competition, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show] 
+  before_action :correct_user, only: [:edit, :update, :destroy]
+respond_to :html, :json 
 
-  respond_to :html, :json 
-  # GET /competitions
-  # GET /competitions.json
-    def index
+  def index
     @competitions = Competition.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 15)
     respond_with(@competitions)
   end
 
-  # GET /competitions/1
-  # GET /competitions/1.json
   def show
+    respond_with(@competition)
   end
 
-  # GET /competitions/new
   def new
-    @competition = Competition.new
+
+      @competition = current_user.competitions.build
+      respond_with(@competition)
+    
+
+
   end
 
-  # GET /competitions/1/edit
   def edit
   end
 
-  # POST /competitions
-  # POST /competitions.json
   def create
-    @competition = Competition.new(competition_params)
-
-    respond_to do |format|
-      if @competition.save
-        format.html { redirect_to @competition, notice: 'Competition was successfully created.' }
-        format.json { render :show, status: :created, location: @competition }
-      else
-        format.html { render :new }
-        format.json { render json: @competition.errors, status: :unprocessable_entity }
-      end
-    end
+    @competition = current_user.competitions.new(competition_params)
+    @competition.save
+    respond_with(@competition)
   end
 
-  # PATCH/PUT /competitions/1
-  # PATCH/PUT /competitions/1.json
   def update
-    respond_to do |format|
-      if @competition.update(competition_params)
-        format.html { redirect_to @competition, notice: 'Competition was successfully updated.' }
-        format.json { render :show, status: :ok, location: @competition }
-      else
-        format.html { render :edit }
-        format.json { render json: @competition.errors, status: :unprocessable_entity }
-      end
-    end
+    @competition.update(competition_params)
+    respond_with(@competition)
   end
 
-  # DELETE /competitions/1
-  # DELETE /competitions/1.json
   def destroy
     @competition.destroy
-    respond_to do |format|
-      format.html { redirect_to competitions_url, notice: 'Competition was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    respond_with(@competition)
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_competition
       @competition = Competition.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    
+
+    def correct_user
+        @competition = current_user.competitions.find_by(id: params[:id])
+        redirect_to competitions_path, notice: "Not authorized to edit this pin" if @competition.nil?
+
+    end
+
     def competition_params
-      params.require(:competition).permit(:name)
+      params.require(:competition).permit(:name,:category, :description, :collegename, 
+        :money, :onOff,:location,:startDate,:endDate,:organizedBy,:linkWebsite,:linkWebsite,:linkFb,:startDateText,:endDateText)
     end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
   def home
+  	@skip_footer = true
+  	@skip_header = true
   end
 end
