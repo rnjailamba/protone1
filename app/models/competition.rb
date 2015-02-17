9598d3217be322d8537415458df8c1b437b95e24
rnjai <rnjailamba12@gmail.com>
2015-02-17 17:00:33 +0530

    Form-reqd fields and *

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,5 +1,10 @@
 class Competition < ActiveRecord::Base
 
+	validates :name,  presence: true, length: { maximum: 50 }
+	validates :description,  presence: true, length: { maximum: 50 }
+	validates :startDateText,  presence: true
+	validates :category,  presence: true
+
 	belongs_to :user 
 	has_many :meetings, class_name:  "Meeting", foreign_key: "attending_competition_id"
 	has_many :participants, through: :meetings,:source => :participant
