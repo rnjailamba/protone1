076c4817b3cd2e57e3c4ab92beff92f074e72cc3
rnjai <rnjailamba12@gmail.com>
2015-03-04 06:16:11 +0530

    one cycle of register/unregister works ok

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -4,23 +4,36 @@ class MeetingsController < ApplicationController
 
 	def create
 		Rails.logger.info params[:attending_competition_id]
+
+		Rails.logger.info params[:id]
 		Rails.logger.info @comp
 
 		@comp  = Competition.find(params[:attending_competition_id])
 		@meeting = current_user.meetings.build(:attending_competition_id => @comp.id)
-		
+		#current_user.meetings.create(params[:id])
+
 		  if @meeting.save
-		    flash[:notice] = "Added friend."
-		    redirect_to root_url
+		    flash[:notice] = "Registered."
+		    
 		  else
-		    flash[:error] = "Unable to add friend."
-		    redirect_to root_url
+		    flash[:error] = "Unable to register."
+		    
+		  end
+
+		  respond_to do |format|
+			  format.html 
+			  format.js
 		  end
 	end
 
 	 def destroy
-	    @comp  = Competition.find(params[:attending_competition_id])
-	    current_user.meetings.find_by(@comp.id).destroy
+	    #@comp  = Competition.find(params[:id])
+	    current_user.meetings.find_by(params[:id]).destroy
+
+	    respond_to do |format|
+		  format.html
+		  format.js
+		end
 	  end
 	
 
